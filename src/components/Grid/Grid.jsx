c011e814ff73260de4e1fb146f0b1e33775dae9d
Rahul Raj <raj.rahul686@gmail.com>
2024-10-20 14:36:06 +0530

    Added the isWinner logic to check who is winning

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import Card from "../Card/Card";
 import "./Grid.css";
+import { isWinner } from "../../utility/isWinner";
+
 const Grid = ({ numberOfCards }) => {
   const [turn, setTurn] = useState(true);
   const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+  const [winner, setWinner] = useState("");
   const play = (position) => {
     console.log("moved played");
     board[position] = turn ? "O" : "X";
     setBoard([...board]);
+    setWinner(isWinner(board, turn ? "O" : "X"));
     setTurn((prevTurn) => !prevTurn);
   };
 
   return (
     <>
+      {winner && <h1 className="turn-highlight">winner is : {winner} </h1>}
       <h1 className="turn-highlight">Current Turn : {(turn && "O") || "X"} </h1>
       <div className="grid_Container">
         {/* {Array(numberOfCards)
